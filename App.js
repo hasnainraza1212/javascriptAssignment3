fd44c224b032e3053bae7b19b5b5318b8cf21e8d
HasnainRaza <105921794+hasnainraza1212@users.noreply.github.com>
2023-02-04 18:39:33 +0500

    chapter 5 just added

---
+++ App.js
@@ -0,0 +1,266 @@
+// .................chapter5..................//
+// question:1
+var num1 = 12;
+var num2 = 13;
+var sum = num1 + num2;
+document.write("the sum of 12 and 13 = "+ sum);
+document.write("<br></br>")
+
+
+// question:2(a)
+var num1 = 12;
+var num2 = 13;
+var sub = num1 - num2;
+document.write("the subtraction of 12 and 13 = "+ sub);
+document.write("<br></br>")
+
+
+// question:2(a)
+var num1 = 12;
+var num2 = 13;
+var multiply = num1 * num2;
+document.write("the multiplication of 12 and 13 = "+ multiply);
+document.write("<br></br>")
+
+
+// question:2(b)
+var num1 = 12;
+var num2 = 13;
+var division = num1 / num2;
+document.write(" 12 divide by 13 = "+ division);
+document.write("<br></br>")
+
+
+// question:2(c)
+var num1 = 12;
+var num2 = 13;
+var modulus = num1 % num2;
+document.write("when 12 divide by 13 we get  = "+ modulus + " as remainder");
+document.write("<br></br>")
+
+
+// question 3(a)
+var num3 = 3;
+// question 3(b)
+document.write("The value of NUM3 = " + num3);
+document.write("<br></br>");
+
+
+// question 3(c)
+num3 =4;
+document.write("The value of NUM3 = " + num3);
+document.write("<br></br>");
+
+
+// question 3(d)
+document.write("The initial value of NUM3 = " + num3);
+document.write("<br></br>");
+
+
+// question 3(e)
+ num3++;
+
+
+// question 3(f)
+document.write("The Incremented value of NUM3 = " + num3);
+document.write("<br></br>");
+
+
+// question 3(g)
+num3 = num3 +7;
+
+
+// question 3(h)
+document.write("when 7 added in NUM3 = " + num3);
+document.write("<br></br>");
+
+
+// question 3(i)
+num3--;
+
+
+// question 3(j)
+document.write("The decremented value of NUM3 = " + num3);
+document.write("<br></br>");
+
+
+// question 3(k)
+num3 = num3 % 3;
+document.write("The remainder  = " + num3);
+document.write("<br></br>");
+
+
+// question 4
+// var costOfTicket = 600;
+// var numberOfTickets = 5;
+// var totalCost = costOfTicket * numberOfTickets;
+// document.write("The cost of five movie tickets = " + totalCost + " PKR");
+// document.write("<br></br>");
+
+// question 5
+var tableOf = 4;
+var countDown = 1;
+var answer = tableOf*countDown;
+document.write(tableOf + " x "+ countDown + " = " + answer);
+document.write("<br></br>");
+
+
+
+++countDown;
+var answer = tableOf*countDown;
+document.write(tableOf + " x "+ countDown + " = " + answer);
+document.write("<br></br>");
+
+++countDown;
+var answer = tableOf*countDown;
+document.write(tableOf + " x "+ countDown + " = " + answer);
+document.write("<br></br>");
+
+++countDown;
+var answer = tableOf*countDown;
+document.write(tableOf + " x "+ countDown + " = " + answer);
+document.write("<br></br>");
+
+++countDown;
+var answer = tableOf*countDown;
+document.write(tableOf + " x "+ countDown + " = " + answer);
+document.write("<br></br>");
+
+++countDown;
+var answer = tableOf*countDown;
+document.write(tableOf + " x "+ countDown + " = " + answer);
+document.write("<br></br>");
+
+++countDown;
+var answer = tableOf*countDown;
+document.write(tableOf + " x "+ countDown + " = " + answer);
+document.write("<br></br>");
+
+++countDown;
+var answer = tableOf*countDown;
+document.write(tableOf + " x "+ countDown + " = " + answer);
+document.write("<br></br>");
+
+++countDown;
+var answer = tableOf*countDown;
+document.write(tableOf + " x "+ countDown + " = " + answer);
+document.write("<br></br>");
+
+++countDown;
+var answer = tableOf*countDown;
+document.write(tableOf + " x "+ countDown + " = " + answer);
+document.write("<br></br>");
+
+// question 5
+
+var Tc;
+var Fc;
+
+// temp in Fahrenheit
+Tc = 27;
+Fc =(Tc-(9/5))+32;
+document.write("Temperature in Fahrenheit " + Fc );
+document.write("<br></br>");
+
+
+// temp in Celcius
+Fc = 57.2;
+Tc = (Fc-32)+(9/5);
+document.write("Temperature in Celcius " + Tc );
+document.write("<br></br>");
+
+
+// question7
+
+var priceOfProductA = 650;
+var quantityOfProductA = 3;
+var priceOfProductB = 100;
+var quantityOfProductB = 7;
+var shippingCharges = 100;
+
+var totalCost = (priceOfProductA*quantityOfProductA) + (priceOfProductB*quantityOfProductB) +shippingCharges;
+document.write("Total cost of order " +totalCost);
+document.write("<br></br>");
+
+
+// question8
+var totalMark = 980;
+var obtainedMarks = 804;
+var percentage = (obtainedMarks/totalMark)*100;
+document.write("percentage = " + percentage + " %");
+document.write("<br></br>");
+
+// question9
+var NumberOfusd;
+var NumberOfRiyal;
+var exchangeRateOfUsd;
+var exchangeRateOfRiyal;
+NumberOfusd = 10;
+NumberOfRiyal = 25;
+exchangeRateOfUsd = 104.80;
+exchangeRateOfRiyal = 28;
+var usdToPkr = NumberOfusd*exchangeRateOfUsd;
+var riyalToPkr = NumberOfRiyal*exchangeRateOfRiyal;
+var totalAmountInPkr = usdToPkr+riyalToPkr;
+document.write("Total amount "+totalAmountInPkr + " PKR");
+document.write("<br></br>");
+
+
+// question10
+var num5 = 0;
+num5 = num5 + 5 * 10 / 2;
+document.write(num5);
+document.write("<br></br>");
+
+
+// question11
+var currentYear = 2023;
+var birthYear = 2000;
+var age = currentYear - birthYear;
+document.write("User's age is " + age);
+document.write("<br></br>");
+
+
+// question12
+var radius;
+radius = 5;
+var circumferrence = 2*3.14*radius;
+var area = 3.14*radius*radius;
+document.write("Circumferrence and Area of a circle whose radius is 2cm found to be" + " " + circumferrence + " and " + area + " respectively!!");
+document.write("<br></br>");
+
+// question13
+var favoriteSnack = "Chiicken Cheese Paratha"
+var snackPerDay;
+snackPerDay = 2;
+var currentge;
+currentge = 23;
+var maximumAge;
+maximumAge = 100;
+var approximatege = maximumAge - currentge;
+var snackTillRestOfLife = approximatege*snackPerDay;
+document.write(snackTillRestOfLife + " " + "Chiicken Cheese Parathas");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
